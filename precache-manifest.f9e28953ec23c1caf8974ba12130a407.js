fbc99aa0b98da534ce802131ede89f1620c60013
Mikheil Sakhelashvili <mishka.sakhelashvili@gmail.com>
2021-03-12 16:21:19 +0400

    Updates

---
+++ precache-manifest.f9e28953ec23c1caf8974ba12130a407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b5aa1ab1decfd6fc58c3eac0aa1706",
+    "url": "/React__VoiceMan/index.html"
+  },
+  {
+    "revision": "6b3a7ebf5b920e878a7c",
+    "url": "/React__VoiceMan/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "67e92b4eb18baf3e6832",
+    "url": "/React__VoiceMan/static/js/2.7cd9b722.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React__VoiceMan/static/js/2.7cd9b722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3a7ebf5b920e878a7c",
+    "url": "/React__VoiceMan/static/js/main.753c55e1.chunk.js"
+  },
+  {
+    "revision": "37bc5272f237f24772de",
+    "url": "/React__VoiceMan/static/js/runtime-main.f52aa1f6.js"
+  }
+]);
